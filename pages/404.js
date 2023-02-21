3b1727570b747092f29032575aa7325688336d7d
Emiliano Viña <emisk805@gmail.com>
2023-02-21 13:29:39 +0100

    correcion de errores

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,14 @@
-import Image from "next/image"
+
 import Link from "next/link"
 import Layout from "@/components/layout"
-import imgerror from "../public/Pagina404.webp"
+
 
 export default function Pagina404() {
     return (
         <Layout>
             <h1 className="titulo">Pagina no encontrada</h1>
             <div className="errorPage">
-                <Image src={imgerror} width={250} height={250} alt={'pagina no encontrada'}/>
+
                 <Link href="/" className="btninfo">
                     Volver a Inicio
                 </Link>
